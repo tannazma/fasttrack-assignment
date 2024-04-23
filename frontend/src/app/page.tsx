327254faa114b5ccb5139f5274a65547d1834205
tannazma <97254921+tannazma@users.noreply.github.com>
2024-04-23 12:19:24 +0200

    make background more beutiful

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,17 +14,24 @@ export default function Home() {
   const handleHolidaySubmit = (holiday: HolidayInterface) => {
     console.log("Submitting holiday:", holiday);
   };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p className="mb-3 text-2xl font-semibold fixed left-50% top-0 flex w-full justify-center border-b border-blue-50 bg-gradient-to-b from-blue-100 pb-6 pt-8 backdrop-blur-2xl">
         Fast Track Assessment
       </p>
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image src="/holiday.jpg" alt="Logo" width={400} height={24} priority />
+      <div className="flex pl-[100px] pt-4 w-[100%] before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial">
+        <Image
+          src="/holiday.jpg"
+          alt="Logo"
+          width={400}
+          height={70}
+          priority
+          className="flex"
+        />
+        <HolidayForm onSubmit={handleHolidaySubmit} />
       </div>
-      <HolidayForm onSubmit={handleHolidaySubmit} />
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-8 left-50% flex h-48 w-full justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none">
+      <ul>
           <p>
             By
             <a
